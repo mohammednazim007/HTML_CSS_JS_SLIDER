65f5c0663f3e44a6973b50639fafadc548059fe2
najima1 <mohammednazim3629@gmail.com>
2023-08-09 03:42:44 -0600

    all code base system changed

--- index.js
+++ index.js
@@ -1,47 +1,43 @@
-const items = document.querySelectorAll(".slider .item")
+
+const nodes = document.querySelectorAll(".item")
 const prev = document.getElementById("prev")
 const next = document.getElementById("next")
 
-
 let active = 3
-const sliderFn = () => {
-    let stt = 0
+const fireFn = () => {
+    let count = 0
 
-    items[active].style.transform = `none`;
-    items[active].style.zIndex = 1
-    items[active].style.filter = `none`
-    items[active].style.opacity = 1
+    nodes[active].style.transform = `none`
+    nodes[active].style.zIndex = 1
+    nodes[active].style.filter = `none`
+    nodes[active].style.opacity = 1;
 
-    //The loop will work after click the next button
-    for (let i = active + 1; i < items.length; i++) {
-        stt++
-        items[i].style.transform = `translateX(${ 120 * stt }px) scale(${ 1 - 0.2 * stt })`;
-        items[i].style.zIndex = -stt
-        items[i].style.filter = `blur(1px)`
-        items[i].style.opacity = stt > 2 ? 0 : 0.6;
+    for (let i = active + 1; i < nodes.length; i++) {
+        count++;
+        nodes[i].style.transform = `translateX(${ 120 * count }px) scale(${ 1 - 0.2 * count }) `
+        nodes[i].style.zIndex = -count
+        nodes[i].style.filter = `blur(2px)`
+        nodes[i].style.opacity = count > 2 ? 0 : 0.6;
     }
 
-        //The loop will work after click the next button
-    stt = 0
+    count = 0
     for (let i = active - 1; i >= 0; i--) {
-        stt++
-        items[i].style.transform = `translateX(${ -120 * stt }px) scale(${ 1 - 0.2 * stt })`;
-        items[i].style.zIndex = -stt
-        items[i].style.filter = `blur(1px)`
-        items[i].style.opacity = stt > 2 ? 0 : 0.6;
+        count++;
+        nodes[i].style.transform = `translateX(${ -120 * count }px) scale(${ 1 - 0.2 * count })`
+        nodes[i].style.zIndex = -count
+        nodes[i].style.filter = `blur(2px)`
+        nodes[i].style.opacity = count > 2 ? 0 : 0.6;
     }
-
 }
 
-// the function will call after loade the window
-sliderFn()
+fireFn()
 
-// 
-next.addEventListener("click", () => {
-    active = active + 1 < items.length ? active + 1 : active
-    sliderFn()
+next.addEventListener('click', () => {
+    active = active + 1 < nodes.length ? active + 1 : active
+    fireFn()
 })
+
 prev.addEventListener("click", () => {
     active = active - 1 >= 0 ? active - 1 : active
-    sliderFn()
+    fireFn()
 })
